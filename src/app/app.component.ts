28db7c26a2085a1e0b6cfa668561dd8224b23736
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-12-11 17:01:40 -0300

    Inicialização do Portinari UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import { PoMenuItem } from '@portinari/portinari-ui';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'economiza-front-portinairi-ui';
+
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Usuário', action: this.onClick.bind(this), icon: 'po-icon-user', shortLabel: 'Usuário' },
+    { label: 'Fontes de Renda', action: this.onClick.bind(this), icon: 'po-icon po-icon-finance-secure', shortLabel: 'Rendas' },
+    { label: 'Gastos', action: this.onClick.bind(this), icon: 'po-icon po-icon-finance', shortLabel: 'Gastos' },
+    { label: 'Categ. de Gasto', action: this.onClick.bind(this), icon: 'po-icon po-icon-list', shortLabel: 'Cat. Gastos' },
+    { label: 'Sair', action: this.onClick.bind(this), icon: 'po-icon po-icon-exit', shortLabel: 'Sair' }
+  ];
+
+  private onClick() {
+    alert('Clicked in menu item')
+  }
+
 }
