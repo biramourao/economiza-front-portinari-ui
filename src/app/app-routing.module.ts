7581fd935962b67f9f5f2261b64bb20d8a118703
biramourao <ubiratan.mourao@gmail.com>
2019-12-12 21:51:52 -0300

    Ajuste nas rotas inesistentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,44 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth/auth.guard';
 
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+/*{
+  path: 'gastos',
+  component: GastosComponent,
+  canActivate: [AuthGuard]
+},
+{ path: '', redirectTo: '/gastos', pathMatch: 'full', canActivate: [AuthGuard] },*/
+/*{ path: 'relatorio', component: GraficosGastosComponent, canActivate: [AuthGuard] },
+{ path: 'not-found', component: NotFoundComponent, canActivate: [AuthGuard] },
+{ path: 'cadastro-usuario', component: CadastroUsuarioComponent },
+{ path: 'detalhes-usuario', component: DetalheCadastroComponent, canActivate: [AuthGuard] },
+{ path: 'categorias-de-gasto', component: CategoriasDeGastoComponent, canActivate: [AuthGuard] },
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+{
+  path: 'categorias-de-gasto/:tipo/:codCategoriaGasto',
+  component: FormCategoriaDeGastoComponent,
+  canActivate: [AuthGuard]
+},
+{
+  path: 'gastos/:tipo/:codGasto',
+  component: FormGastoComponent,
+  canActivate: [AuthGuard]
+},
+{
+  path: 'fontes-de-renda',
+  component: FontesDeRendaComponent,
+  canActivate: [AuthGuard]
+},
+{
+  path: 'fontes-de-renda/:tipo/:codFonteDeRenda',
+  component: FormFonteDeRendaComponent,
+  canActivate: [AuthGuard]
+},*/
+{ path: '**', redirectTo: '/', canActivate: [AuthGuard] }
+//{ path: '**', redirectTo: '/login' }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
