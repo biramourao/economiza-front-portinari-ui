01b4d0852de70d9a7cf8b32940ccf2fcffaffa15
Ubiratan Leitão Mourão <ubiratan.mourao@gmail.com>
2019-12-19 17:35:16 -0300

    Remoção de modulos não usados.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PoModule, PoLoginComponent } from '@portinari/portinari-ui';
+import { PoModule, PoHttpInterceptorService, PoNotificationService } from '@portinari/portinari-ui';
 import { RouterModule } from '@angular/router';
 import { ApiService } from './api.service';
 import { AuthGuard } from './auth/auth.guard';
@@ -14,15 +14,17 @@ import { FormsModule } from '@angular/forms';
 import { PoFieldModule } from '@portinari/portinari-ui';
 import { GastosComponent } from './gastos/gastos.component';
 import { PoPageModule } from '@portinari/portinari-ui';
-import { PoPageLoginModule, PoModalPasswordRecoveryComponent } from '@portinari/portinari-templates';
 import { IndicadoresComponent } from './indicadores/indicadores.component';
+import { PoTableModule } from '@portinari/portinari-ui';
+import { PoHttpInterceptorModule } from '@portinari/portinari-ui';
+import { PoNotificationModule } from '@portinari/portinari-ui';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     GastosComponent,
-    IndicadoresComponent,
-
+    IndicadoresComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +33,9 @@ import { IndicadoresComponent } from './indicadores/indicadores.component';
     FormsModule,
     PoFieldModule,
     routing,
-    PoPageModule
+    PoPageModule,
+    PoTableModule
+
     
   ],
   providers: [
