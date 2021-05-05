064936c36c2b668dc55d840c94925b3277bfa0e0
Ubiratan Leitao Mourao <ulmourao@latam.stefanini.com>
2021-05-05 16:54:15 -0300

    Correção de Erros relacionados a migração de Versão

--- src/app/form-gastos/form-gastos.component.ts
+++ src/app/form-gastos/form-gastos.component.ts
@@ -1,4 +1,9 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { CategoriaGasto } from '../model/categoria-gasto';
+import { Gasto } from '../model/gasto';
 
 @Component({
   selector: 'app-form-gastos',
@@ -7,9 +12,160 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormGastosComponent implements OnInit {
 
-  constructor() { }
+  titleForm: string;
+  gasto = new Gasto();
+  dataVencimentoEdit: string;
+  categoriasGasto = new Array<CategoriaGasto>();
+  categoriaDeGasto = new CategoriaGasto();
+  mostrarCadastroCategoriaDeGasto = false;
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private router: Router, private apiService: ApiService) {
+    this.route.params.subscribe(
+      res => {
+        switch (res.tipo) {
+          case 'cadastrar': {
+            this.titleForm = 'Cadastrar gasto';
+            this.gasto.nome = '';
+            this.categoriaDeGasto.descricao = '';
+            break;
+          }
+          case 'editar': {
+            if (res.codGasto) {
+              this.titleForm = 'Editar gasto';
+              this.categoriaDeGasto.descricao = '';
+              this.getGasto(res.codGasto);
+            } else {
+              this.router.navigate(['/not-found']);
+            }
+            break;
+          }
+          default: {
+            this.router.navigate(['/gastos']);
+          }
+        }
+      }
+    );
+    this.listCategoriaGasto();
+  }
+
+  ngOnInit(
+
+  ) {
+  }
+
+  onSubmit() {
+    switch (this.titleForm) {
+      case 'Cadastrar gasto': {
+        this.cadastrarGasto();
+        break;
+      }
+      case 'Editar gasto': {
+        this.editarGasto();
+        break;
+      }
+    }
+  }
+
+  cadastrarGasto() {
+
+    this.gasto.cod = null;
+
+    if (this.gasto.categoriaGasto === null) {
+      this.gasto.categoriaGasto = null;
+    }
+
+    this.apiService.cadastrarGasto(this.gasto).subscribe(
+      data => {
+        console.log('O Gasto ' + data.nome + ' foi cadastrada com sucesso!');
+        alert('O Gasto ' + data.nome + ' foi cadastrada com sucesso!');
+        this.router.navigate(['/gastos']);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  editarGasto() {
+
+    if (this.gasto.categoriaGasto.cod === null) {
+      this.gasto.categoriaGasto = null;
+    }
+
+    this.apiService.editarGasto(this.gasto).subscribe(
+      data => {
+        console.log('O Gasto ' + data.nome + ' foi editada com sucesso!');
+        alert('O Gasto ' + data.nome + ' foi editada com sucesso!');
+        this.router.navigate(['/gastos']);
+
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  getGasto(cod: any) {
+    this.apiService.getGasto(cod).subscribe(
+      data => {
+        this.gasto = data;
+        this.gasto.vencimento = formatDate(data.vencimento, 'yyyy-MM-dd', 'en-US', '-03:00');
+        if (this.gasto.dtPagamento) {
+          this.gasto.dtPagamento = formatDate(data.dtPagamento, 'yyyy-MM-dd', 'en-US', '-03:00');
+        }
+        if (data.categoriaGasto === null) {
+          this.gasto.categoriaGasto = new CategoriaGasto();
+        }
+      },
+      error => {
+        console.log(error);
+        this.router.navigate(['/gastos']);
+      }
+    );
+  }
+  listCategoriaGasto() {
+    this.apiService.listCategoriaDeGasto().subscribe(
+      data => {
+        this.categoriasGasto = data as unknown as CategoriaGasto[];
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+  cadastrarCategoriaGasto() {
+    this.categoriaDeGasto.cod = null;
+    this.apiService.cadastrarCategoriaGasto(this.categoriaDeGasto).subscribe(
+      data => {
+        alert('A Categoria ' + data.descricao + ' foi cadastrada com sucesso!');
+        this.listCategoriaGasto();
+        this.mostrarCadastroCategoria();
+        //$('#CadastroCategoriaDeGastoModal').modal('hide');
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  mostrarCadastroCategoria() {
+    if (this.mostrarCadastroCategoriaDeGasto) {
+      this.mostrarCadastroCategoriaDeGasto = false;
+    } else {
+      this.mostrarCadastroCategoriaDeGasto = true;
+    }
+  }
+  ativaForm() {
+    if (this.gasto.nome.search(/([a-zA-Z0-9-])/g) === 0 && this.gasto.vencimento.search(/([0-9][0-9][0-9][0-9])([ \-])(0?[1-9]|1[012])([ \-])(0?[1-9]|[12][0-9]|3[01])/g) === 0 && this.gasto.categoriaGasto.cod > 0) {
+      return false;
+
+    } else {
+      return true;
+    }
+  }
+  ativaFormCategoriaGasto() {
+    if (this.categoriaDeGasto.descricao.search(/([a-zA-Z0-9-])/g) === 0) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
 }
