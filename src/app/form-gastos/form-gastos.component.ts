93a5b4a57682c940c93cdf73e4b237bfee3c78df
biramourao <ubiratan.mourao@gmail.com>
2022-01-19 17:08:29 -0300

    [WIP] formulario dinamico de gasto

--- src/app/form-gastos/form-gastos.component.ts
+++ src/app/form-gastos/form-gastos.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Gasto} from "../model/gasto";
+import {PoDynamicFormField, PoNotificationService} from "@portinari/portinari-ui";
 
 @Component({
   selector: 'app-form-gastos',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormGastosComponent implements OnInit {
 
-  constructor() { }
+  gasto: Gasto = new Gasto();
+  /*
+  cod: number;
+	nome: string;
+	valor: number;
+	vencimento: any;
+	qtdParcelas: number;
+	dtPagamento: any;
+	categoriaGasto = new CategoriaGasto();
+	cartaoDeCredito: CartaoDeCredito;
+	*/
+
+  fields: Array<PoDynamicFormField> = [
+    {
+      property: 'nome',
+      label: 'Nome',
+      required: true,
+      minLength: 1,
+      gridColumns: 6,
+      gridSmColumns: 12
+    },
+    {
+      property: 'valor',
+      label: 'Valor',
+      type: 'currency',
+      gridColumns: 6,
+      gridSmColumns: 12,
+      errorMessage: 'erro'
+    },
+    {
+      property: 'vencimento',
+      label: 'Vencimento',
+      type: 'date',
+      gridColumns: 6,
+      gridSmColumns: 12,
+      errorMessage: 'The date must be before the year 2010.'
+    }
+  ];
+
+  constructor(public poNotification: PoNotificationService) {
+  }
 
   ngOnInit() {
+  }
+
+  salvar() {
+    console.log(this.gasto);
   }
 
 }
