a69945c6f2eea879e41746b901c42e07ce0905b3
biramourao <ubiratan.mourao@gmail.com>
2022-01-20 19:39:05 -0300

    [WIP] implementação do cadastro de gasto

--- src/app/form-gastos/form-gastos.component.ts
+++ src/app/form-gastos/form-gastos.component.ts
@@ -1,57 +1,100 @@
 import {Component, OnInit} from '@angular/core';
 import {Gasto} from "../model/gasto";
-import {PoDynamicFormField, PoNotificationService} from "@po-ui/ng-components";
+import {PoDynamicFormField, PoNotificationService, PoSelectComponent, PoSelectOption} from "@po-ui/ng-components";
+import {CategoriaGasto} from "../model/categoria-gasto";
+import {ApiService} from "../api.service";
+import {map} from "rxjs";
 
 @Component({
   selector: 'app-form-gastos',
   templateUrl: './form-gastos.component.html',
   styleUrls: ['./form-gastos.component.css']
 })
-export class FormGastosComponent implements OnInit {
+export class FormGastosComponent implements OnInit{
 
   gasto: Gasto = new Gasto();
-  /*
-  cod: number;
-	nome: string;
-	valor: number;
-	vencimento: any;
-	qtdParcelas: number;
-	dtPagamento: any;
-	categoriaGasto = new CategoriaGasto();
-	cartaoDeCredito: CartaoDeCredito;
-	*/
+  categorias: Array<PoSelectOption>;
+  fields: Array<PoDynamicFormField>;
 
-  fields: Array<PoDynamicFormField> = [
-    {
-      property: 'nome',
-      label: 'Nome',
-      required: true,
-      minLength: 1,
-      gridColumns: 6,
-      gridSmColumns: 12
-    },
-    {
-      property: 'valor',
-      label: 'Valor',
-      type: 'currency',
-      gridColumns: 6,
-      gridSmColumns: 12,
-      errorMessage: 'erro'
-    },
-    {
-      property: 'vencimento',
-      label: 'Vencimento',
-      type: 'date',
-      gridColumns: 6,
-      gridSmColumns: 12,
-      errorMessage: 'The date must be before the year 2010.'
-    }
-  ];
-
-  constructor(public poNotification: PoNotificationService) {
+  constructor(public poNotification: PoNotificationService, private readonly apiService: ApiService) {
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.obterCategoriasDeGasto();
+    this.initForm();
+  }
+
+  async obterCategoriasDeGasto() {
+    this.categorias = new Array<PoSelectOption>();
+    await this.apiService.listCategoriaDeGasto().pipe(
+      map(categorias => {
+        categorias.map(
+            categoria => {
+              let tempCategoria = {
+                label: categoria.descricao,
+                value: categoria.cod
+              } as PoSelectOption
+              this.categorias.push(tempCategoria);
+              console.log(this.categorias);
+            }
+          )
+        }
+      )
+    ).subscribe()
+  }
+  initForm(){
+    this.fields = [
+      {
+        property: 'nome',
+        label: 'Nome',
+        required: true,
+        placeholder: 'Digite a descrição',
+        minLength: 1,
+        gridColumns: 6,
+        gridSmColumns: 12,
+        order: 1
+      },
+      {
+        property: 'valor',
+        label: 'Valor',
+        type: 'currency',
+        icon: 'po-icon-finance',
+        gridColumns: 6,
+        gridSmColumns: 12,
+        errorMessage: 'erro',
+        required: true,
+        placeholder: 'Digite o valor',
+        order: 2
+      },
+      {
+        property: 'vencimento',
+        label: 'Vencimento',
+        type: 'date',
+        gridColumns: 6,
+        gridSmColumns: 12,
+        placeholder: 'Selecione o vencimento',
+        format: 'DD/MM/YYYY',
+        required: true
+      },
+      {
+        property: 'dtPagamento',
+        label: 'Pagamento',
+        type: 'date',
+        gridColumns: 6,
+        gridSmColumns: 12,
+        placeholder: 'Selecione a data de pagamento',
+        format: 'DD/MM/YYYY',
+        required: true
+      },
+      {
+        property: 'categoriaGasto.id',
+        label: 'Categoria',
+        placeholder: 'Selecione a categoria',
+        gridColumns: 6,
+        options: this.categorias
+      }
+
+    ];
   }
 
   salvar() {
